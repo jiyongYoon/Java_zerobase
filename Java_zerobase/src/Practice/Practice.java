cbcd9308206fa73e154c4769de741d5cd339297e
jiyongYoon <jyyoon0615@naver.com>
2022-05-10 16:54:37 +0900

    220510 머지 테스트

--- Java_zerobase/src/Practice/Practice.java
+++ Java_zerobase/src/Practice/Practice.java
@@ -3,6 +3,7 @@ package Practice;
 // 줄 추가하면 어떻게 되는지?
 public class Practice {
 	public static void main(String[] args) {
+		System.out.println("메인은 이거다!!!");
 		System.out.println("브랜치 생성!");
 	}
 }
